5aea2d407b9f32858697bf86b8a3c4d7a0d4c4e0
Quinton <quintonjon16@gmail.com>
2024-06-13 22:24:52 -0500

    set up position sizing

--- tradingbot.py
+++ tradingbot.py
@@ -3,7 +3,7 @@
 # torch: pytorch framework for using AI
 # transformers: load up finance deep learning model
 
-import os
+import math
 from lumibot.brokers import Alpaca # broker
 from lumibot.backtesting import YahooDataBacktesting # framework for testing
 from lumibot.strategies.strategy import Strategy # trading bot
@@ -24,22 +24,38 @@ ALPACA_CREDS = {
 # Holds all trading logic
 class MLTrader(Strategy):
     # Initialized once at beginning (setup logic)
-    def initialize(self, symbol:str="SPY"):
+    def initialize(self, symbol:str="SPY", cash_at_risk:float=0.5):
         self.symbol = symbol # Index of traded stock
         self.sleeptime = "24H" # Frequency of trades
         self.last_trade = None # Last trade
+        self.cash_at_risk = cash_at_risk # Percent of cash at risk
+
+    # Amount of cash that will be invested
+    def position_sizing(self):
+        cash = self.get_cash() # Amount of cash in account
+        last_price = self.get_last_price(self.symbol)
+
+        if last_price != 0:
+            quantity = math.floor(cash * self.cash_at_risk / last_price) # Amount of cash at risk
+        return cash, last_price, quantity
+
 
     # Runs everytime new data is received (trading logic)
     def on_trading_iteration(self):
-        if self.last_trade == None:
-            order = self.create_order(
-                self.symbol,
-                10,
-                "buy",
-                type="market"
-            )
-            self.submit_order(order)
-            self.last_trade = "buy"
+        cash, last_price, quantity = self.position_sizing()
+
+        if cash > last_price: # Check that there is enough cash in account to make purchase
+            if self.last_trade == None:
+                order = self.create_order(
+                    self.symbol,
+                    quantity,
+                    "buy",
+                    type="bracket",
+                    take_profit_price = last_price * 1.2, # Sell any profit when up 20%
+                    stop_loss_limit_price = last_price * 0.95 # Sell any losses when down 5%
+                )
+                self.submit_order(order)
+                self.last_trade = "buy"
 
 # Initialize start and end dates
 start_date = datetime(2024, 4, 20)
@@ -51,10 +67,12 @@ broker = Alpaca(ALPACA_CREDS)
 # Instatiate trader
 strategy = MLTrader(name='mlstrat',
                     broker=broker, 
-                    parameters={"symbol":"SPY"}) 
+                    parameters={"symbol":"SPY",
+                                "cash_at_risk":0.5}) 
 
 # Instatiate back tester
 strategy.backtest(YahooDataBacktesting,
                   start_date, 
                   end_date, 
-                  parameters={"symbol":"SPY"})
+                  parameters={"symbol":"SPY",
+                              "cash_at_risk":0.5})
